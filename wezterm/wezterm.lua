f8f3096437bf903aa3c5388a40b136bd2234cfdc
AntsaJudicael <aleck.dev.git@gmail.com>
2025-03-28 18:42:17 +0300

    set custom  font in wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,13 +1,34 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-
 ---Colorscheme
 config.color_scheme = "Atom One Dark"
 
 --- Font settings
-config.font_size = 22
--- config.font = wezterm.font("Maple Mono NF")
+config.font_size = 23
+config.harfbuzz_features = { "calt", "liga", "clig", "dlig" }
+config.line_height = 1
+-- config.font = wezterm.font("Maple Mono NF", { weight = 500 })
 config.font = wezterm.font("Cascadia Code NF")
+config.font_rules = {
+	{
+		intensity = "Normal",
+		italic = false,
+		font = wezterm.font({
+			family = "Cascadia Code NF",
+			italic = false,
+			weight = 400,
+		}),
+	},
+	{
+		intensity = "Normal",
+		italic = true,
+		font = wezterm.font({
+			family = "Victor Mono",
+			italic = true,
+			weight = 600,
+		}),
+	},
+}
 
 --- Colors
 config.colors = {
@@ -21,7 +42,7 @@ config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
 	left = 25,
 	right = 15,
-	top = 10,
+	top = 15,
 	bottom = 0,
 }
 -- config.window_background_image = constants.bg_image
@@ -33,7 +54,7 @@ config.background = {
 	-- },
 	{
 		source = {
-			Color = "rgba(10, 14, 20, 0.6)",
+			Color = "rgba(10, 14, 20, 0.4)",
 			-- Color = "#0A0E14",
 		},
 		height = "100%",
