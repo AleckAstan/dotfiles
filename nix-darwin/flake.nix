5353ab4b3fb351ae2d739886dd536a3e5d9523ca
Antsa Judicael <antsajudicael@MacBook-Pro.local>
2026-01-03 19:29:50 +0300

    format

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -1,150 +1,157 @@
 {
-    description = "My nix-darwin system flake";
+  description = "My nix-darwin system flake";
 
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-        nix-darwin.url = "github:nix-darwin/nix-darwin/master";
-        nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-        nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
 
-        homebrew-core = {
-            url = "github:homebrew/homebrew-core";
-            flake = false;
-        };
-        homebrew-cask = {
-            url = "github:homebrew/homebrew-cask";
-            flake = false;
-        };
- asmvik-formulae = {
-    url = "github:asmvik/homebrew-formulae";
-    flake = false;
-  };
-
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+    asmvik-formulae = {
+      url = "github:asmvik/homebrew-formulae";
+      flake = false;
     };
 
-    outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, homebrew-core, homebrew-cask, asmvik-formulae,home-manager}:
-        let
-        configuration = { pkgs, ... }: {
-            nixpkgs.config.allowUnfree = true;
-# List packages installed in system profile. To search by name, run:
-# $ nix-env -qaP | grep wget
-            environment.systemPackages =
-                [ 
-                pkgs.btop
-                    pkgs.git
-                    pkgs.kitty
-                    pkgs.neovim
-                    pkgs.tmux
-                    pkgs.fzf
-                    pkgs.ripgrep
-                    pkgs.yazi
-                    pkgs.lazygit
-		    pkgs.starship
-		    pkgs.zoxide
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+      nix-homebrew,
+      homebrew-core,
+      homebrew-cask,
+      asmvik-formulae,
+      home-manager,
+    }:
+    let
+      configuration =
+        { pkgs, ... }:
+        {
+          nixpkgs.config.allowUnfree = true;
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = [
+            pkgs.nixfmt-rfc-style
+            pkgs.btop
+            pkgs.git
+            pkgs.kitty
+            pkgs.neovim
+            pkgs.tmux
+            pkgs.fzf
+            pkgs.ripgrep
+            pkgs.yazi
+            pkgs.lazygit
+            pkgs.starship
+            pkgs.zoxide
             pkgs.asdf-vm
-                ];
+          ];
 
+          system.primaryUser = "antsajudicael";
 
-            system.primaryUser = "antsajudicael";
+          users.users.antsajudicael = {
+            shell = pkgs.fish;
+          };
+          homebrew = {
+            enable = true;
 
+            casks = [
+              "iina"
+              "the-unarchiver"
+              "raycast"
+            ];
+            brews = [
+              "yabai"
+              "skhd"
+            ];
+            onActivation.cleanup = "zap";
+          };
 
-users.users.antsajudicael = {
-  shell = pkgs.fish;
-};
-            homebrew = {
-                enable = true;
+          # users.users.antsajudicael = {
+          #     shell = pkgs.fish;
+          # };
 
+          system.defaults = {
+            dock.autohide = true;
+            dock.persistent-apps = [
+              "${pkgs.kitty}/Applications/kitty.app"
+              "/Applications/Zen.app"
+              "/System/Applications/Music.app"
+            ];
+            finder._FXSortFoldersFirst = true;
+            finder.FXPreferredViewStyle = "clmv";
+            finder.ShowPathbar = true;
+            trackpad.Clicking = true;
+            trackpad.TrackpadRightClick = true;
+            loginwindow.GuestEnabled = false;
+            # NSGlobalDomain."com.apple.swipescrolldirection" = true;
+            NSGlobalDomain._HIHideMenuBar = true;
+            NSGlobalDomain.AppleICUForce24HourTime = true;
+            NSGlobalDomain.AppleInterfaceStyle = "Dark";
+            WindowManager.StandardHideDesktopIcons = true;
+          };
 
-                casks = [
-                    "iina"
-                        "the-unarchiver"
-			"raycast"
-                ];
- brews = [
-    "yabai"
-    "skhd"
-  ];
-                onActivation.cleanup = "zap";
-            };
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
 
+          # Enable alternative shell support in nix-darwin.
+          programs.fish.enable = true;
 
-            # users.users.antsajudicael = {
-            #     shell = pkgs.fish;
-            # };
+          environment.shells = [ pkgs.fish ];
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
 
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 6;
 
-system.defaults= {
-dock.autohide = true;
-dock.persistent-apps = [
-"${pkgs.kitty}/Applications/kitty.app"
-"/Applications/Zen.app"
-"/System/Applications/Music.app"
-];
-finder._FXSortFoldersFirst = true;
-finder.FXPreferredViewStyle = "clmv";
-finder.ShowPathbar = true;
-trackpad.Clicking = true;
-trackpad.TrackpadRightClick = true;
-loginwindow.GuestEnabled = false;
-# NSGlobalDomain."com.apple.swipescrolldirection" = true; 
-NSGlobalDomain._HIHideMenuBar = true;
-NSGlobalDomain.AppleICUForce24HourTime = true;
-NSGlobalDomain.AppleInterfaceStyle = "Dark";
-WindowManager.StandardHideDesktopIcons= true;
-};
-
-# Necessary for using flakes on this system.
-            nix.settings.experimental-features = "nix-command flakes";
-
-# Enable alternative shell support in nix-darwin.
-            programs.fish.enable = true;
-
-
-environment.shells = [ pkgs.fish ];
-# Set Git commit hash for darwin-version.
-            system.configurationRevision = self.rev or self.dirtyRev or null;
-
-# Used for backwards compatibility, please read the changelog before changing.
-# $ darwin-rebuild changelog
-            system.stateVersion = 6;
-
-# The platform the configuration will be used on.
-            nixpkgs.hostPlatform = "aarch64-darwin";
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
 
         };
     in
     {
-# Build darwin flake using:
-# $ darwin-rebuild build --flake .#mbp
-        darwinConfigurations."mbp" = nix-darwin.lib.darwinSystem {
-            modules = [ 
-                configuration 
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#mbp
+      darwinConfigurations."mbp" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
 
-                nix-homebrew.darwinModules.nix-homebrew
-                {
-                    nix-homebrew = {
-# Install Homebrew under the default prefix
-                        enable = true;
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              # Install Homebrew under the default prefix
+              enable = true;
 
-# Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-                        enableRosetta = true;
+              # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+              enableRosetta = true;
 
-# User owning the Homebrew prefix
-                        user = "antsajudicael";
+              # User owning the Homebrew prefix
+              user = "antsajudicael";
 
-# Optional: Declarative tap management
-                        taps = {
-                            "homebrew/homebrew-core" = homebrew-core;
-    "homebrew/homebrew-cask" = homebrew-cask;
-"asmvik/homebrew-formulae" = asmvik-formulae;
-                        };
+              # Optional: Declarative tap management
+              taps = {
+                "homebrew/homebrew-core" = homebrew-core;
+                "homebrew/homebrew-cask" = homebrew-cask;
+                "asmvik/homebrew-formulae" = asmvik-formulae;
+              };
 
-# Optional: Enable fully-declarative tap management
-#
-# With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
-                        mutableTaps = false;
-                    };
-                }		  ];
-        };
+              # Optional: Enable fully-declarative tap management
+              #
+              # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+              mutableTaps = false;
+            };
+          }
+        ];
+      };
     };
 }
--- nvim/lua/plugins/format.lua
+++ nvim/lua/plugins/format.lua
@@ -1,20 +1,21 @@
 return {
-	"stevearc/conform.nvim",
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				javascript = { "prettier", "eslint-lsp" },
-				javascriptreact = { "prettier", "eslint-lsp" },
-				typescript = { "prettier", "eslint-lsp" },
-				typescriptreact = { "prettier", "eslint-lsp" },
-				json = { "prettier" },
-				yaml = { "prettier" },
-				lua = { "stylua" },
-				swift = { "swiftformat" },
-			},
-			format_on_save = {
-				lsp_format = "fallback",
-			},
-		})
-	end,
+    "stevearc/conform.nvim",
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {
+                javascript = { "prettier", "eslint-lsp" },
+                javascriptreact = { "prettier", "eslint-lsp" },
+                typescript = { "prettier", "eslint-lsp" },
+                typescriptreact = { "prettier", "eslint-lsp" },
+                json = { "prettier" },
+                yaml = { "prettier" },
+                lua = { "stylua" },
+                swift = { "swiftformat" },
+                nix = { "nixfmt" },
+            },
+            format_on_save = {
+                lsp_format = "fallback",
+            },
+        })
+    end,
 }
